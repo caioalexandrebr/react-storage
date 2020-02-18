2d15eda3a19b7980b43e39390ab7757a04609ff4
Caio Alexandre <caioalexandre.br@gmail.com>
2020-02-18 16:44:36 -0300

    feat(List): retrieving data on localstorage

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,25 +1,65 @@
 import React, { Component } from 'react';
+import styled from "styled-components";
 import axios from 'axios';
 
+const Group = styled.ul`
+  padding: 20px;
+  margin: 0 auto;
+  max-width: 250px;
+  background: white;
+  border-radius: 5px;
+
+  li {
+    padding: 10px;
+    font-size: 20px;
+    list-style: none;
+  }
+`;
+
 export default class List extends Component {
-  state = {
-    repos: []
+  constructor(props) {
+    super(props);
+    this.state = {
+      repos: [],
+      isLoading: true
+    }
+  }
+
+  fetchData() {
+    axios.get(`https://api.github.com/users/caioalexandrebr/repos`)
+    .then(res => this.setState({
+      repos: res.data,
+      isLoading: false
+    }))
+  }
+
+  componentWillMount() {
+    localStorage.getItem('repos') && this.setState({
+      isLoading: false,
+      repos: JSON.parse(localStorage.getItem('repos'))
+    })
   }
 
   componentDidMount() {
-    axios.get(`https://api.github.com/users/caioalexandrebr/repos`)
-    .then(res => {
-      console.log(res.data);
-      this.setState({ repos: res.data })
-    });
+    if(localStorage.getItem('repos')) {
+      console.log('Using data from localStorage.');
+    } else {
+      this.fetchData();
+      console.log('Fetch.');
+    }
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    localStorage.setItem('repos', JSON.stringify(nextState.repos));
+    localStorage.setItem('reposDate', Date.now());
   }
   
   render() {
     return (
       <div>
-        <ul>
+        <Group  >
           { this.state.repos.map(repo => <li key={repo.id}>{repo.name}</li>)}
-        </ul>
+        </Group >
       </div>
     );
   }
